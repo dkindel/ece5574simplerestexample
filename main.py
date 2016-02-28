a1bb8ef98f405076f7cb3e6eb003be7c244643af
Dave Kindel <dkindel@vt.edu>
2016-02-28 09:54:51 -0500

    updated PUT

--- main.py
+++ main.py
@@ -22,39 +22,43 @@ robots = [
     }
 ]
 
-@app.route('/todo/api/v1.0/robots', methods=['GET'])
+@app.route('/', methods=['GET'])
+def get_home():
+    return "To access the API, navigate to /api/robots"
+
+@app.route('/api/robots', methods=['GET'])
 def get_robots():
     return jsonify({'robots': robots})
 
-@app.route('/todo/api/v1.0/robots/<int:r_id>', methods=['GET'])
+@app.route('/api/robots/<int:r_id>', methods=['GET'])
 def get_robot(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
         abort(404)
     return jsonify({'robot': robot[0]})
 
-@app.route('/todo/api/v1.0/robots/status/<int:r_id>', methods=['GET'])
+@app.route('/api/robots/status/<int:r_id>', methods=['GET'])
 def get_robot_status(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
         abort(404)
     return jsonify({'robot': robot[0]['status']})
 
-@app.route('/todo/api/v1.0/robots/floor/<int:r_id>', methods=['GET'])
+@app.route('/api/robots/floor/<int:r_id>', methods=['GET'])
 def get_robot_floor(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
         abort(404)
     return jsonify({'robot': robot[0]['floor']})
 
-@app.route('/todo/api/v1.0/robots/room/<int:r_id>', methods=['GET'])
+@app.route('/api/robots/room/<int:r_id>', methods=['GET'])
 def get_robot_room(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
         abort(404)
     return jsonify({'robot': robot[0]['room']})
 
-@app.route('/todo/api/v1.0/robots', methods=['POST'])
+@app.route('/api/robots', methods=['POST'])
 def create_robot():
     if len(robots) == 0:
        	newbot = {
@@ -77,28 +81,40 @@ def create_robot():
     robots.append(newbot)
     return jsonify({'robot': newbot}), 201
 
-@app.route('/todo/api/v1.0/robots/<int:r_id>', methods=['PUT'])
+@app.route('/api/robots/<int:r_id>', methods=['PUT'])
 def update_robot(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
-    if len(task) == 0:
+    if len(robot) == 0:
         abort(404)
-    if not request.json:
+    data = request.get_json()
+    if not data:
         abort(400)
-    if 'floor' in request.json and type(request.json['floor']) is not int:
-        abort(400)
-    if 'room' in request.json and type(request.json['room']) is not int:
-        abort(400)
-    if 'attacker' in request.json and type(request.json['attacker']) is not bool:
-        abort(400)
-    if 'status' in request.json and type(request.json['status']) is not int:
-        abort(400)
-    robot[0]['floor'] = request.json.get('floor', task[0]['floor'])
-    robot[0]['room'] = request.json.get('room', task[0]['room'])
-    robot[0]['attacker'] = request.json.get('attacker', task[0]['attacker'])
-    robot[0]['status'] = request.json.get('status', task[0]['status'])
+    if 'floor' in data:
+        if type(data['floor']) is not int:
+            abort(400)
+        else:
+            robot[0]['floor'] = data['floor']
+
+    if 'room' in data:
+        if type(data['room']) is not int:
+            abort(400)
+        else:
+            robot[0]['room'] = data['room']
+
+    if 'attacker' in data:
+        if type(data['attacker']) is not bool:
+            abort(400)
+        else:
+            robot[0]['attacker'] = data['attacker']
+
+    if 'status' in data:
+        if type(data['status']) is not int:
+            abort(400)
+        else:
+            robot[0]['status'] = data['status']
     return jsonify({'robot': robot[0]})
 
-@app.route('/todo/api/v1.0/robots/<int:r_id>', methods=['DELETE'])
+@app.route('/api/robots/<int:r_id>', methods=['DELETE'])
 def delete_robot(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
@@ -106,7 +122,7 @@ def delete_robot(r_id):
     robots.remove(robot[0])
     return jsonify({'result': True})
 
-@app.route('/todo/api/v1.0/robots', methods=['DELETE'])
+@app.route('/api/robots', methods=['DELETE'])
 def delete_robots():
     robots.remove(robot[:])
     return jsonify({'result': True})
