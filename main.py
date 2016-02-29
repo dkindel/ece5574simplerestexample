8d4ea5448c440a1cb906bfb817d7bed48faf82ab
Dave Kindel <dkindel@vt.edu>
2016-02-28 23:11:09 -0500

    Add custom error handling

--- main.py
+++ main.py
@@ -47,38 +47,38 @@ robots = [
 def set_robot(robot):
     data = request.get_json()
     if not data:
-        abort(400)
+        abort(400, "No data in the request")
     if 'floor' in data:
         if type(data['floor']) is not int:
-            abort(400)
+            abort(400, "\'floor\' isn't an integer")
         else:
             robot['floor'] = data['floor']
 
     if 'room' in data:
         if type(data['room']) is not int:
-            abort(400)
+            abort(400, "\'room\' isn't an integer")
         else:
             robot['room'] = data['room']
 
     if 'attacker' in data:
         if type(data['attacker']) is not bool:
-            abort(400)
+            abort(400, "\'attacker\' isn't a bool")
         else:
             robot['attacker'] = data['attacker']
 
     if 'status' in data:
         if type(data['status']) is not int:
-            abort(400)
+            abort(400, "\'status\' isn't an integer")
         else:
             robot['status'] = data['status']
 
     if 'sensors' in data:
         if not isinstance(data['sensors'], list):
-            abort(400)
+            abort(400, "\'sensors\' isn't a list")
         #check to make sure they're all ints
         for sensor_id in data['sensors']:
             if 'id' not in sensor_id or type(sensor_id['id']) is not int:
-                abort(400)
+                abort(400, "\'id\' isn't defined for the sensor")
         #We replace the ENTIRE list, not just add sensors
         del robot['sensors'][:]
         for sensor_id in data['sensors']:
@@ -90,7 +90,7 @@ def set_robot(robot):
 
     if 'building' in data:
         if "id" not in data['building'] or type(data['building']['id']) is not int:
-            abort(400)
+            abort(400, "\'id\' isn't defined for the building")
         new_bldg = {
                 "id": data['building']['id'],
                 "ref": "/api/buildings/" + str(data['building']['id'])+"/"
@@ -110,28 +110,28 @@ def get_robots():
 def get_robot(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     return jsonify({'robot': robot[0]})
 
 @app.route('/api/robots/<int:r_id>/status/', methods=['GET'])
 def get_robot_status(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     return jsonify({'status': robot[0]['status']})
 
 @app.route('/api/robots/<int:r_id>/floor/', methods=['GET'])
 def get_robot_floor(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     return jsonify({'floor': robot[0]['floor']})
 
 @app.route('/api/robots/<int:r_id>/room/', methods=['GET'])
 def get_robot_room(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     return jsonify({'room': robot[0]['room']})
 
 
@@ -139,7 +139,7 @@ def get_robot_room(r_id):
 def get_robot_building(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     return redirect(robot[0]['building']['ref'])
 
 @app.route('/api/buildings/<int:bldg_id>/', methods=['GET'])
@@ -152,7 +152,7 @@ def get_buidling(bldg_id):
 def get_robot_sensors(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     return jsonify({'sensors': robot[0]['sensors']})
 
 
@@ -160,13 +160,13 @@ def get_robot_sensors(r_id):
 def get_robot_sensor(r_id, snsr_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     sensors = robot[0]['sensors']
     if len(sensors) == 0:
-        abort(404)
+        abort(404, "Could not find the sensor with the provided id")
     sensor = [sensor for sensor in sensors if sensor['id'] == snsr_id]
     if len(sensor) == 0:
-        abort(404)
+        abort(404, "Could not find the sensor with the provided id")
     return redirect(sensor[0]['ref'])
 
 @app.route('/api/sensors/<int:snsr_id>/', methods=['GET'])
@@ -179,11 +179,11 @@ def get_sensor(snsr_id):
 def remove_sensor(r_id, snsr_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     sensors = robot[0]['sensors']
     sensor = [sensor for sensor in sensors if sensor['id'] == snsr_id]
     if len(sensor) == 0:
-        abort(404)
+        abort(404, "Could not find the sensor with the provided id")
     sensors.remove(sensor[0])
     return jsonify({'sensors': sensors})
 
@@ -195,17 +195,17 @@ def remove_sensor(r_id, snsr_id):
 def add_sensor(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the provided id in the body")
     sensors = robot[0]['sensors']
     if not data or 'id' not in data or type(data['id']) is not int:
-        abort(400)
+        abort(400, "Could not find \"id\" in the body")
 
     sensor = {
             'id': data['id'],
             'ref': "/api/sensors/"+str(data['id'])+"/"
             }
     sensors.append(sensor)
-    return jsonify({'sensor': sensor})
+    return jsonify({'sensor': sensor}), 201
 
 
 
@@ -247,7 +247,7 @@ def create_robot():
 def update_robot(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     set_robot(robot[0])
     return jsonify({'robot': robot[0]})
 
@@ -256,7 +256,7 @@ def update_robot(r_id):
 def delete_robot(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
-        abort(404)
+        abort(404, "Could not find the robot with the provided id")
     robots.remove(robot[0])
     return jsonify({'result': True})
 
@@ -273,10 +273,35 @@ def custom404(error):
     return response
 
 @app.errorhandler(400)
-def custom404(error):
+def custom400(error):
     response = jsonify({'message': error.description})
     response.status_code = 400
     return response
+
+@app.errorhandler(500)
+def custom500(error):
+    response = jsonify({'message': error.description})
+    response.status_code = 500
+    return response
+
+@app.errorhandler(415)
+def custom415(error):
+    response = jsonify({'message': error.description})
+    response.status_code = 415
+    return response
+
+@app.errorhandler(405)
+def custom405(error):
+    response = jsonify({'message': error.description})
+    response.status_code = 405
+    return response
+
+@app.errorhandler(302)
+def custom302(error):
+    response = jsonify({'message': error.description})
+    response.status_code = 302
+    return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
