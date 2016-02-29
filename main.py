c8405e08df5a6f4bf4ff54c8bc9e6a78af50b687
Dave Kindel <dkindel@vt.edu>
2016-02-28 22:37:07 -0500

    added some more methods to sensors and started work on custom errors to get better details

--- main.py
+++ main.py
@@ -4,45 +4,45 @@ from flask import Flask, jsonify, abort, request, redirect
 app = Flask(__name__)
 
 robots = [
-    {
-        'id': 1,
-        'floor': 1,
-        'room': 1, 
-        'attacker': False, 
-        'status': 1, 
-        'movement': 1,
-        'sensors':[
+        {
+            'id': 1,
+            'floor': 1,
+            'room': 1, 
+            'attacker': False, 
+            'status': 1, 
+            'movement': 1,
+            'sensors':[
+                {
+                    "id": 1,
+                    "ref": "/api/sensors/1/"
+                    },
+                {
+                    "id": 2,
+                    "ref": "/api/sensors/2/"
+                    }
+                ],
+            'building': 
+            {
+                "id": "1",
+                "ref": "/api/buildings/1/"
+                }
+            },
+        {
+            'id': 2,
+            'floor': 1,
+            'room': 2, 
+            'attacker': True, 
+            'status': 1, 
+            'movement': 1,
+            'sensors':[
+                ],
+            'building': 
             {
                 "id": 1,
-                "ref": "/api/sensors/1"
-            },
-            {
-                "id": 2,
-                "ref": "/api/sensors/2"
+                "ref": "/api/buildings/1/"
+                }
             }
-        ],
-        'building': 
-        {
-            "id": "1",
-            "ref": "/api/buildings/1"
-        }
-    },
-    {
-        'id': 2,
-        'floor': 1,
-        'room': 2, 
-        'attacker': True, 
-        'status': 1, 
-        'movement': 1,
-        'sensors':[
-        ],
-        'building': 
-        {
-            "id": 1,
-            "ref": "/api/buildings/1"
-        }
-    }
-]
+        ]
 
 def set_robot(robot):
     data = request.get_json()
@@ -84,8 +84,8 @@ def set_robot(robot):
         for sensor_id in data['sensors']:
             new_sensor = {
                     "id": sensor_id['id'],
-                    "ref": "/api/sensors/"+str(sensor_id['id'])
-            }
+                    "ref": "/api/sensors/"+str(sensor_id['id'])+"/"
+                    }
             robot['sensors'].append(new_sensor)
 
     if 'building' in data:
@@ -93,11 +93,12 @@ def set_robot(robot):
             abort(400)
         new_bldg = {
                 "id": data['building']['id'],
-                "ref": "/api/buildings/" + str(data['building']['id'])
-        }
+                "ref": "/api/buildings/" + str(data['building']['id'])+"/"
+                }
         robot['building'] = new_bldg
 
 @app.route('/', methods=['GET'])
+@app.route('/api/', methods=['GET'])
 def get_home():
     return "To access the API, navigate to /api/robots"
 
@@ -112,26 +113,26 @@ def get_robot(r_id):
         abort(404)
     return jsonify({'robot': robot[0]})
 
-@app.route('/api/robots/status/<int:r_id>/', methods=['GET'])
+@app.route('/api/robots/<int:r_id>/status/', methods=['GET'])
 def get_robot_status(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
         abort(404)
-    return jsonify({'robot': robot[0]['status']})
+    return jsonify({'status': robot[0]['status']})
 
-@app.route('/api/robots/floor/<int:r_id>/', methods=['GET'])
+@app.route('/api/robots/<int:r_id>/floor/', methods=['GET'])
 def get_robot_floor(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
         abort(404)
-    return jsonify({'robot': robot[0]['floor']})
+    return jsonify({'floor': robot[0]['floor']})
 
-@app.route('/api/robots/room/<int:r_id>/', methods=['GET'])
+@app.route('/api/robots/<int:r_id>/room/', methods=['GET'])
 def get_robot_room(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
         abort(404)
-    return jsonify({'robot': robot[0]['room']})
+    return jsonify({'room': robot[0]['room']})
 
 
 @app.route('/api/robots/<int:r_id>/building/', methods=['GET'])
@@ -152,8 +153,8 @@ def get_robot_sensors(r_id):
     robot = [robot for robot in robots if robot['id'] == r_id]
     if len(robot) == 0:
         abort(404)
-    return jsonify({'robot': robot[0]['sensors']})
-    
+    return jsonify({'sensors': robot[0]['sensors']})
+
 
 @app.route('/api/robots/<int:r_id>/sensors/<int:snsr_id>/', methods=['GET'])
 def get_robot_sensor(r_id, snsr_id):
@@ -174,40 +175,71 @@ def get_sensor(snsr_id):
     return "This is where the sensor json stuff for " +str(snsr_id) + " will go!"
 
 
+@app.route('/api/robots/<int:r_id>/sensors/<int:snsr_id>/', methods=['DELETE'])
+def remove_sensor(r_id, snsr_id):
+    robot = [robot for robot in robots if robot['id'] == r_id]
+    if len(robot) == 0:
+        abort(404)
+    sensors = robot[0]['sensors']
+    sensor = [sensor for sensor in sensors if sensor['id'] == snsr_id]
+    if len(sensor) == 0:
+        abort(404)
+    sensors.remove(sensor[0])
+    return jsonify({'sensors': sensors})
+
+
+
+
+#Add a sensor with POST - keeps other sensors added intact
+@app.route('/api/robots/<int:r_id>/sensors/', methods=['POST'])
+def add_sensor(r_id):
+    robot = [robot for robot in robots if robot['id'] == r_id]
+    if len(robot) == 0:
+        abort(404)
+    sensors = robot[0]['sensors']
+    if not data or 'id' not in data or type(data['id']) is not int:
+        abort(400)
+
+    sensor = {
+            'id': data['id'],
+            'ref': "/api/sensors/"+str(data['id'])+"/"
+            }
+    sensors.append(sensor)
+    return jsonify({'sensor': sensor})
 
 
 
 @app.route('/api/robots/', methods=['POST'])
 def create_robot():
     if len(robots) == 0:
-       	newbot = {
-		'id': 1,
-		'floor': 1,
-		'room': 1, 
-		'attacker': False, 
-		'status': 1, 
-		'movement': 1,
+        newbot = {
+                'id': 1,
+                'floor': 1,
+                'room': 1, 
+                'attacker': False, 
+                'status': 1, 
+                'movement': 1,
                 'sensors': [],
                 'building': {
                     'id': 1,
-                    'ref': '/api/buildings/1'
+                    'ref': '/api/buildings/1/'
+                    }
                 }
-    	}
     else:
-    	newbot = {
-		'id': robots[-1]['id']+1,
-		'floor': 1,
-		'room': 1, 
-		'attacker': False, 
-		'status': 1, 
-	        'movement': 1,
+        newbot = {
+                'id': robots[-1]['id']+1,
+                'floor': 1,
+                'room': 1, 
+                'attacker': False, 
+                'status': 1, 
+                'movement': 1,
                 'sensors': [],
                 'building': {
                     'id': 1,
-                    'ref': '/api/buildings/1'
+                    'ref': '/api/buildings/1/'
+                    }
                 }
-    	}
-    set_robot(newbot)
+        set_robot(newbot)
     robots.append(newbot)
     return jsonify({'robot': newbot}), 201
 
@@ -232,6 +264,19 @@ def delete_robot(r_id):
 def delete_robots():
     robots.remove(robot[:])
     return jsonify({'result': True})
+
+
+@app.errorhandler(404)
+def custom404(error):
+    response = jsonify({'message': error.description})
+    response.status_code = 404
+    return response
+
+@app.errorhandler(400)
+def custom404(error):
+    response = jsonify({'message': error.description})
+    response.status_code = 400
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
